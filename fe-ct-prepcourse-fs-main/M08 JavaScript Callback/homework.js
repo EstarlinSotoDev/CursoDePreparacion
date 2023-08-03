47baedb3da7749738643f57ce4ad7b611e03596c
EstarlinSotoDev <estarlinsotodev@gmail.com>
2023-08-03 15:53:55 -0400

    Modulo 8 terminado

--- fe-ct-prepcourse-fs-main/M08 JavaScript Callback/homework.js
+++ fe-ct-prepcourse-fs-main/M08 JavaScript Callback/homework.js
@@ -5,6 +5,7 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   return nombre.toUppercase(0);
 }
 
 function invocarCallback(cb) {
